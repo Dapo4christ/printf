c469da68f14d6e0c5a3954f2999f71671c36f1cd
Dapo4christ <99693614+Dapo4christ@users.noreply.github.com>
2022-04-14 11:01:01 +0100

    Update convert_hex.c

--- convert_hex.c
+++ convert_hex.c
@@ -1,9 +1,4 @@
-/*
- * File: convert_hex.c
- * Authors: Okonkwo Godspower
- */
-
-#include "holberton.h"
+#include "main.h"
 
 unsigned int convert_x(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
