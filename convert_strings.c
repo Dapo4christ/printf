085c38ddf93df36e90e0f4eb9807bf63747fe1a0
Dapo4christ <99693614+Dapo4christ@users.noreply.github.com>
2022-04-14 10:59:39 +0100

    Update convert_strings.c

--- convert_strings.c
+++ convert_strings.c
@@ -1,9 +1,6 @@
-/*
- * File: convert_strings.c
- * Authors: Okonkwo Godspower
- */
 
-#include "holberton.h"
+
+#include "main.h"
 
 unsigned int convert_s(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
